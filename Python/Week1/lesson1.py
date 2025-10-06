a070afb8e586c2923dad1518c6a6ecf69a46a8e7
Kamila Kay <kkalch@Kamilas-Laptop.local>
2025-10-05 20:56:44 -0400

    python(chores):Orgnzing file structure

---
+++ Python/Week1/lesson1.py
@@ -0,0 +1,8 @@
+def my_function():
+    print("Hello From My Function!")
+
+def some_function_from_me():
+    print("Somebody called my function!")
+    
+some_function_from_me() #I am calling my function from here
+my_function()
