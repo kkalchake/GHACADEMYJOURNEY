5655d56140c6630fdfbb44d7a70f1e118dbb74df
Kamila Kay <kkalch@Kamilas-Laptop.local>
2025-10-09 22:16:11 -0400

    week2(python):Project - Pseudo Code

--- Python/Week2/tipcalc/tipcalc.py
+++ Python/Week2/tipcalc/tipcalc.py
@@ -11,3 +11,23 @@
 
 # Author: Kamila Kalchakeeva
 # https://github.com/kkalchake
+
+### Get the user inputs
+# bill_amount = float(input(('What is the bill amount?))
+# tip_percentage = float(input(('What is the tip percentage you wish to add?'))
+
+### Calculate and output
+# float total_bill = bill_amount + (bill_amount*tip_percentage)
+# print ('The total bill with %{tip_percentage} is %{total_bill}')
+
+### Get the user inputs
+# number_of_people = int(input('How many people are splitting the check?'))
+# in range of number_of_people:
+#     name = input('What is your name?')
+#     ordered_for = list(input('Enter coma separated format dollar amounts for each dish you ordered))
+# takes bill_amount -> calculates all people's order total and assigns tax_amount
+# splits tax_amount + tip_amount
+# adds above to sum of each dish ordered
+### Output whole math
+# Output table with each name and how much each owe
+# Output confirmation of right math
