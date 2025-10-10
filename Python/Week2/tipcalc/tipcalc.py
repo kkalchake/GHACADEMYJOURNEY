2a6fbca3041a0bf34740dbd22f27a38b787d1b35
Kamila Kay <kkalch@Kamilas-Laptop.local>
2025-10-09 22:03:41 -0400

    python(week2):tipCalc authorship

---
+++ Python/Week2/tipcalc/tipcalc.py
@@ -0,0 +1,13 @@
+# Basic Tip Calculator. 
+# Write a program that
+#      a) asks for the total bill amount
+#      b) asks for the tip percentage. 
+#      c) calculates and displays:
+#           1 ) the tip amount
+#           2)  the total bill including the tip.
+# Advanced: 
+#      d) asks for a name and dollar amounts of dishes comma separated
+#      e) print the total amount owed with name on it.
+
+# Author: Kamila Kalchakeeva
+# https://github.com/kkalchake
