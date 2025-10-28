c815cacc721dfda588691b8d54c2968566cb0e9b
Kamila Kay <kkalch@Kamilas-Laptop.local>
2025-10-27 21:20:56 -0400

    week5(python):catching up w homewrk

---
+++ Python/Week3/list1.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python3
+# Basic string exercises
+
+
+# Given an int count of a number of donuts, return a string
+# of the form 'Number of donuts: <count>', where <count> is the number
+# passed in. However, if the count is 10 or more, then use the word 'many'
+# instead of the actual count.
+# So donuts(5) returns 'Number of donuts: 5'
+# and donuts(23) returns 'Number of donuts: many'
+# A. donuts
+def donuts(count):
+  message_template = 'Number of donuts: '
+  if count < 10:
+    message_template += str(count)
+  else:
+    message_template += 'many'
+  return message_template
+
+# Given a string s, return a string made of the first 2
+# and the last 2 chars of the original string,
+# so 'spring' yields 'spng'. However, if the string length
+# is less than 2, return instead the empty string.
+# B. both_ends
+def both_ends(s):
+  if len(s) > 2:
+    new_string = s[:2] + s[-2:]
+  else:
+    new_string = ''
+  return new_string
+
+# C. fix_start
+# Given a string s, return a string
+# where all occurences of its first char have
+# been changed to '*', except do not change
+# the first char itself.
+# e.g. 'babble' yields 'ba**le'
+# Assume that the string is length 1 or more.
+# Hint: s.replace(stra, strb) returns a version of string s
+# where all instances of stra have been replaced by strb.
+# C. fix_start
+def fix_start(s):
+  if len(s) > 1:
+    stra = s[0]
+    strb = '*'
+    replace_occurences = s.replace(stra,strb)
+    new_string = stra + replace_occurences[1:]
+  else: 
+    new_string = ''
+  return new_string
+
+# Given strings a and b, return a single string with a and b separated
+# by a space '<a> <b>', except swap the first 2 chars of each string.
+# e.g.
+#   'mix', pod' -> 'pox mid'
+#   'dog', 'dinner' -> 'dig donner'
+# Assume a and b are length 2 or more.
+# D. MixUp
+def mix_up(a, b):
+  if len(a) > 2 and len(b) > 2:
+    single_string = b[0:2] + a[1] + ' ' + a[0:2] + b[0]
+  return single_string
+
+def test(got, expected):
+  if got == expected:
+    prefix = ' OK '
+  else:
+    prefix = '  X '
+  print('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
+
+def main():
+  print('donuts')
+  # Each line calls donuts, compares its result to the expected for that call.
+  test(donuts(4), 'Number of donuts: 4')
+  test(donuts(9), 'Number of donuts: 9')
+  test(donuts(10), 'Number of donuts: many')
+  test(donuts(99), 'Number of donuts: many')
+
+  print()
+  print('both_ends')
+  test(both_ends('spring'), 'spng')
+  test(both_ends('Hello'), 'Helo')
+  test(both_ends('a'), '')
+  test(both_ends('xyz'), 'xyyz')
+
+
+  print()
+  print('fix_start')
+  test(fix_start('babble'), 'ba**le')
+  test(fix_start('aardvark'), 'a*rdv*rk')
+  test(fix_start('google'), 'goo*le')
+  test(fix_start('donut'), 'donut')
+
+  print()
+  print('mix_up')
+  test(mix_up('mix', 'pod'), 'pox mid')
+  test(mix_up('dog', 'dinner'), 'dig donner')
+  test(mix_up('gnash', 'sport'), 'spash gnort')
+  test(mix_up('pezzy', 'firm'), 'fizzy perm')
+
+
+# Standard boilerplate to call the main() function.
+if __name__ == '__main__':
+  main()
