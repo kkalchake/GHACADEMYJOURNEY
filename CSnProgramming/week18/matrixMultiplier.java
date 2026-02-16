f368b9e1f0b7eaa223c653ad6e8cb89de777ed67
Kamila Kay <kkalch@Kamilas-Laptop.local>
2026-02-15 20:49:16 -0500

    homeWork(java):Matrix Multiplier

--- CSnProgramming/week18/matrixMultiplier.java
+++ CSnProgramming/week18/matrixMultiplier.java
@@ -5,37 +5,61 @@ import java.util.Random;
 
 class MatrixMultiplier
 {
-	static final int ROWS = 3;
-	static final int COLS = 3;
-	static final int MAX_RANDOM_VALUE = 10;
-	
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		int[][] matrixA = makeTwoDMatrixWithRandomValues();
-		int[][] matrixB = makeTwoDMatrixWithRandomValues();
-		
-		System.out.println("Matrix A: ");
-		printMatrix(matrixA);
-		
-		System.out.println("Matrix B: ");
-		printMatrix(matrixB);
-		
-		int[][] multipliedMatrix = multiplyMatrices(matrixA, matrixB);
-		
-		System.out.println("\nResult Matrix: ");
-		printMatrix(multipliedMatrix);
-		
-	}
-	
-	public static int[][] makeTwoDMatrixWithRandomValues() {
-		
-	}
-	
-	public static int[][] muliplyMatrices(){
-		
-	}
-	
-	public static void printMatrix(int[][] matrix) {
-		
-	}
+  static final int ROWS = 3;
+  static final int COLS = 3;
+  static final int MAX_RANDOM_VALUE = 10;
+
+  public static void main (String[] args) throws java.lang.Exception
+  {
+    int[][] matrixA = makeTwoDMatrixWithRandomValues();
+    int[][] matrixB = makeTwoDMatrixWithRandomValues();
+
+    System.out.println("Matrix A: ");
+    printMatrix(matrixA);
+
+    System.out.println("Matrix B: ");
+    printMatrix(matrixB);
+
+    int[][] multipliedMatrix = multiplyMatrices(matrixA, matrixB);
+
+    System.out.println("\nResult Matrix: ");
+    printMatrix(multipliedMatrix);
+
+  }
+
+  public static int[][] makeTwoDMatrixWithRandomValues() {
+    int[][] matrix = new int[ROWS][COLS];
+    Random randomNumber = new Random();
+
+    for (int i = 0; i < ROWS; i++) {
+      for (int j = 0; j < COLS; j++) {
+        matrix[i][j] = randomNumber.nextInt(MAX_RANDOM_VALUE);
+      }
+    }
+    return matrix;
+  }
+
+  public static int[][] multiplyMatrices(int[][]matrixA, int[][] matrixB){
+    int[][] resultMatrix = new int[ROWS][COLS];
+
+    for (int i = 0; i < ROWS; i++) {
+      for (int j = 0; j < COLS; j++) {
+        int sum = 0;
+        for (int k = 0; k < COLS; k++) {
+          sum += matrixA[i][k] * matrixB[k][j];
+        }
+        resultMatrix[i][j] = sum;
+      }
+    }
+    return resultMatrix;
+  }
+
+  public static void printMatrix(int[][] matrix) {
+    for (int i = 0; i < ROWS; i++) {
+      for (int j = 0; j < COLS; j++) {
+        System.out.print(matrix[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
 }
